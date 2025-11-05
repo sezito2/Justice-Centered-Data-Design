a89722d71323da2dceeb554eff67a62267a1768c
sezito2 <142633286+sezito2@users.noreply.github.com>
2025-11-05 15:10:54 -0500

    Update utils.js

--- src/03-Textual-Analysis/utils/utils.js
+++ src/03-Textual-Analysis/utils/utils.js
@@ -21,24 +21,6 @@ export const sparkbar = (max) => {
     justify-content: end;">${x.toLocaleString("en-US")}`
 }
 
-/** getUniquePropListBy()
- * Goal: Create a unique list of `x` property
- *       in an array of objects.
- * @params
- *   - arr: Array. Any array of objects.
- *   - key: String. Desired property to isolate.
- * @return
- *   - uniqList: Array. List of unique data values.
-**/
-export const getUniquePropListBy = (arr, key) => {
-  const uniqueObjs = [...new Map(arr.map(item => [item[key], item])).values()]
-  const uniqList = []
-  for (const o of uniqueObjs) {
-    uniqList.push(o[key])
-  }
-  return uniqList
-}
-
 export const printList = (doc) => {
   return JSON.stringify(doc.out('array'), null, 2)
 }
